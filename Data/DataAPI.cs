0efe5b38038eb20c839830b27f351ea6ce701ec1
Rafal G <75140476+rafalgorniak@users.noreply.github.com>
2022-05-24 12:50:55 +0200

    1commitUpdate README.md

---
+++ Data/DataAPI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Data.API
+{
+    public interface IRepository
+    {
+        Task<IBook> GetBook(int id);
+        Task<IEnumerable<IBook>> GetBooks();
+        Task AddBook(int id, string title, string author);
+        Task UpdateBook(int id, string title, string author);
+        Task DeleteBook(int id);
+        Task<IState> GetState(int id);
+        Task<IEnumerable<IState>> GetBookStates(int id);
+        Task<IEnumerable<IState>> GetStates();
+        Task AddState(int id, int book_id, string available);
+        Task UpdateState(int id, int book_id, string available);
+        Task DeleteState(int id);
+        Task<IUser> GetUser(int id);
+        Task<IEnumerable<IUser>> GetUsers();
+        Task AddUser(int id, string name, string surname);
+        Task UpdateUser(int id, string name, string surname);
+        Task DeleteUser(int id);
+        Task<IEvent> GetEvent(int id);
+        Task<IEnumerable<IEvent>> GetStateEvents(int id);
+        Task<IEnumerable<IEvent>> GetUserEvents(int id);
+        Task<IEnumerable<IEvent>> GetEvents();
+        Task AddEvent(int id, int state_id, int user_id, string type);
+        Task UpdateEvent(int id, int state_id, int user_id, string type);
+        Task DeleteEvent(int id); 
+    }
+
+    public abstract class DataRepositoryFactory
+    {
+        public static IRepository CreateRepository()
+        {
+            return new Repository();
+        }
+    }
+
+    public interface IBook
+    {
+        int id { get; set; }
+        string title { get; set; }
+        string author { get; set; }
+    }
+
+    public interface IState
+    {
+        int id { get; set; }
+        int book_id { get; set; }
+        string available { get; set; }
+    }
+
+    public interface IUser
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string surname { get; set; }
+    }
+
+    public interface IEvent
+    {
+        int id { get; set; }
+        int state_id { get; set; }
+        int user_id { get; set; }
+        string type { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# PTv2
-Programming Technologies
+# Programming Technology Lab
+
+## Team 12.15
+
+| Name Surname (initials) | GUID                                     |
+| ----------------------- | ---------------------------------------- |
+| Rafał Górniak           | '{165f8f5d-e2c2-4e5a-97b8-50c893b9f429}' |
